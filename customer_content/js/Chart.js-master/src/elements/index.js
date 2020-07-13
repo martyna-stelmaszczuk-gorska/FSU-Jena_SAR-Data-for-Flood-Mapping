af37248d698a4b76366c4ad8c33c5e092dee6623
simon-donike <simon-donike@users.noreply.github.com>
2020-07-13 11:49:57 +0200

    del chart.js

--- customer_content/js/Chart.js-master/src/elements/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Arc} from './element.arc';
-export {default as Line} from './element.line';
-export {default as Point} from './element.point';
-export {default as Rectangle} from './element.rectangle';
